842ec9994634b5f549b18a3112ed8e05e76ac7cf
camp <qianbeiyouli@gmail.com>
2018-12-19 13:07:41 +0800

    测试

--- japar/db.py
+++ japar/db.py
@@ -1,7 +1,6 @@
 import pymysql.cursors
 from flask import g
 import os
-import configparser
 
 
 def get_db():
@@ -9,23 +8,14 @@ def get_db():
     is unique for each request and will be reused if this is called
     again.
     """
-    from urllib.parse import urlparse
-    url = urlparse(os.environ['CLEARDB_DATABASE_URL'])
-    print(url)
-    print(url.netloc)
-    config = configparser.ConfigParser()
-    conf = config.read("../config/database.conf")
-    if conf is None:
-        conf = config.read("./config/database.conf")
-        if conf is None:
-            conf = config.read("/config/database.conf")
-            if conf is None:
-                conf = config.read("config/database.conf")
-    print(conf)
-    mysql = config["MySQL"]
     try:
         from boto.s3.connection import S3Connection
     except ImportError:
+        import configparser
+        config = configparser.ConfigParser()
+        config.read("../config/database.conf")
+
+        mysql = config["MySQL"]
         host = mysql["host"]
         port = mysql["port"]
         user = mysql["user"]
@@ -35,20 +25,22 @@ def get_db():
         from urllib.parse import urlparse
         if 'CLEARDB_DATABASE_URL' in os.environ:
             url = urlparse(os.environ['CLEARDB_DATABASE_URL'])
-            print(url)
-            host = url["host"]
+            dbconf = url.netloc
+            host = dbconf.split("@")[1]
             port = 3306
-            user = url["user"]
-            password = url["password"]
-            db = mysql["db"]
-    if 'db' not in g:
-        g.db = pymysql.connect(host=host,
-                               port=port,
-                               user=user,
-                               password=password,
-                               db=db,
-                               charset='utf8mb4',
-                               cursorclass=pymysql.cursors.DictCursor)
+            user = dbconf.split("@")[0].split(":")[1]
+            password = dbconf.split(":")
+            db = url.path[1:]
+            print(dbconf, host, user, password, db)
+    finally:
+        if 'db' not in g:
+            g.db = pymysql.connect(host=host,
+                                   port=port,
+                                   user=user,
+                                   password=password,
+                                   db=db,
+                                   charset='utf8mb4',
+                                   cursorclass=pymysql.cursors.DictCursor)
 
     return g.db
 
